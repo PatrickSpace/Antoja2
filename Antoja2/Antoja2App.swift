0443878beaebd301e06e2df0b22f0bb974baa807
Patrick Lostaunau <jplosta01@gmail.com>
2026-08-24 19:04:58 -0500

    Initial Commit

---
+++ Antoja2/Antoja2App.swift
@@ -0,0 +1,17 @@
+//
+//  Antoja2App.swift
+//  Antoja2
+//
+//  Created by Patrick Lostaunau on 24/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Antoja2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
